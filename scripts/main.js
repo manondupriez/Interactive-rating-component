c12ff60b516d60b2af5ad5dfc873b32634aad03f
Manon Dupriez <manondupriez@outlook.fr>
2023-05-13 15:21:33 +0200

    Add JS logic

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,31 @@ const setRatingBar = (minScore, maxScore) => {
     }
 }
 setRatingBar(1, 5)
-console.log(ratingBar.length)
 
+const selectRating = () => {
+    ratingBar.forEach(element => {
+        element.addEventListener('click', () => {
+            ratingBar.forEach(item => {
+                item.classList.remove('selected');
+            })
+            element.classList.add('selected')
+        })
+    })
+}
+selectRating()
+
+
+const submitRating = () => {
+    document.getElementById('submit-button').addEventListener('click', () => {
+        ratingBar.forEach((element, index) => {
+            if (element.classList.contains('selected')) {
+                document.getElementById('rating-area').classList.add('d-none')
+                document.getElementById('thanks-area').classList.remove('d-none')
+                document.getElementById('user-rating-value').innerText = index + 1
+            } else {
+                document.getElementById('error').classList.remove('d-none')
+            }
+        })
+    })
+}
+submitRating()
